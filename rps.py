729cbd98aae1e7f2d80070ba8e449d41573bd9f0
Gavin Quirk <gavinquirk@gmail.com>
2020-05-21 14:28:43 -0700

    removed redundant code

--- rps.py
+++ rps.py
@@ -1,7 +1,7 @@
 import random
 
 # Possible choices
-choices = ['rock', 'paper', 'scissors']
+choices = ["rock", "paper", "scissors"]
 
 # Inputs
 user_input = input()
@@ -13,23 +13,23 @@ lose_output = f"Sorry, but computer chose {computer_choice}"
 tie_output = f"There is a draw ({computer_choice})"
 
 # Winning conditions
-if user_input == "scissors" and computer_choice == 'paper':
-    print(win_output)
-elif user_input == "rock" and computer_choice == "scissors":
-    print(win_output)
-elif user_input == "paper" and computer_choice == "rock":
+if (
+    (user_input == "scissors" and computer_choice == "paper") or
+    (user_input == "rock" and computer_choice == "scissors") or
+    (user_input == "paper" and computer_choice == "rock")
+):
     print(win_output)
 # Losing conditions
-elif user_input == "paper" and computer_choice == 'scissors':
-    print(lose_output)
-elif user_input == "scissors" and computer_choice == "rock":
-    print(lose_output)
-elif user_input == "rock" and computer_choice == "paper":
+elif (
+    (user_input == "paper" and computer_choice == "scissors") or
+    (user_input == "scissors" and computer_choice == "rock") or
+    (user_input == "rock" and computer_choice == "paper")
+):
     print(lose_output)
 # Tie conditions
-elif user_input == "paper" and computer_choice == 'paper':
-    print(tie_output)
-elif user_input == "scissors" and computer_choice == "scissors":
-    print(tie_output)
-elif user_input == "rock" and computer_choice == "rock":
+elif (
+    (user_input == "paper" and computer_choice == "paper") or
+    (user_input == "scissors" and computer_choice == "scissors") or
+    (user_input == "rock" and computer_choice == "rock")
+):
     print(tie_output)
