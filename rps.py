9b7450546d947ab6fb844662ffd03aeae4f7428a
Gavin Quirk <gavinquirk@gmail.com>
2020-05-29 12:38:00 -0700

    continuous game, exit, handle invalid input

--- rps.py
+++ rps.py
@@ -3,33 +3,46 @@ import random
 # Possible choices
 choices = ["rock", "paper", "scissors"]
 
-# Inputs
-user_input = input()
-computer_choice = random.choice(choices)
 
-# Outputs
-win_output = f"Well done. Computer chose {computer_choice} and failed"
-lose_output = f"Sorry, but computer chose {computer_choice}"
-tie_output = f"There is a draw ({computer_choice})"
+while True:
+    # Choices for this round
+    computer_choice = random.choice(choices)
+    user_input = input("Enter your choice: ")
 
-# Winning conditions
-if (
-    (user_input == "scissors" and computer_choice == "paper") or
-    (user_input == "rock" and computer_choice == "scissors") or
-    (user_input == "paper" and computer_choice == "rock")
-):
-    print(win_output)
-# Losing conditions
-elif (
-    (user_input == "paper" and computer_choice == "scissors") or
-    (user_input == "scissors" and computer_choice == "rock") or
-    (user_input == "rock" and computer_choice == "paper")
-):
-    print(lose_output)
-# Tie conditions
-elif (
-    (user_input == "paper" and computer_choice == "paper") or
-    (user_input == "scissors" and computer_choice == "scissors") or
-    (user_input == "rock" and computer_choice == "rock")
-):
-    print(tie_output)
+    # Outputs
+    win_output = f"Well done. Computer chose {computer_choice} and failed"
+    lose_output = f"Sorry, but computer chose {computer_choice}"
+    tie_output = f"There is a draw ({computer_choice})"
+
+    # Exit condition
+    if (
+        user_input == "!exit"
+    ):
+        print("Bye!")
+        break
+    # Invalid user input
+    elif (
+        user_input not in choices
+    ):
+        print("Invalid input")
+    # Winning conditions
+    elif (
+        (user_input == "scissors" and computer_choice == "paper") or
+        (user_input == "rock" and computer_choice == "scissors") or
+        (user_input == "paper" and computer_choice == "rock")
+    ):
+        print(win_output)
+    # Losing conditions
+    elif (
+        (user_input == "paper" and computer_choice == "scissors") or
+        (user_input == "scissors" and computer_choice == "rock") or
+        (user_input == "rock" and computer_choice == "paper")
+    ):
+        print(lose_output)
+    # Tie conditions
+    elif (
+        (user_input == "paper" and computer_choice == "paper") or
+        (user_input == "scissors" and computer_choice == "scissors") or
+        (user_input == "rock" and computer_choice == "rock")
+    ):
+        print(tie_output)
